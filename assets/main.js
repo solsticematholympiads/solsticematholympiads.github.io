dc5d75043d4d1c3d6bd024675ee60ba9351086f1
Anthony Zou <107489530+loggamma@users.noreply.github.com>
2025-12-05 12:26:47 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,10 @@
-
+fetch("../assets/includes/header.html")
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById("header-container").innerHTML = html;
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 10) header.classList.add('scrolled');
+      else header.classList.remove('scrolled');
+    });
+  });
