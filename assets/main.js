ba22ab208cccf0221e55558fbc36844bcfae9f5d
Anthony Zou <107489530+loggamma@users.noreply.github.com>
2025-12-06 20:28:26 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 /* ==== IMPORTS ==== */
-fetch("assets/includes/header.html")
+fetch("/assets/includes/header.html")
   .then(response => response.text())
   .then(html => {
     document.getElementById("header-container").innerHTML = html;
